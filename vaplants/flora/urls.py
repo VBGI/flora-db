362a60f1d7d41799c0d8bc9925350e5355d4e20a
Kislov Dmitry <kislov@easydan.com>
2019-10-07 11:31:05 +1000

    Occurrence detail view added

--- vaplants/flora/urls.py
+++ vaplants/flora/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from flora.views import (FamilyDetailView, GenusDetailView, SpeciesDetailView,
                         FamilyListView, GenusListView, SpeciesListView, 
-                        SearchView, PageView)
+                        SearchView, PageView, OccurrenceDetailView)
 
 urlpatterns = [
     path('family/<int:pk>', FamilyDetailView.as_view(), name='family-detail'),
     path('genus/<int:pk>', GenusDetailView.as_view(), name='genus-detail'),
     path('species/<int:pk>', SpeciesDetailView.as_view(), name='species-detail'),
+    path('occurrence/<int:pk>', OccurrenceDetailView.as_view(), name='occurrence-detail'),
     path('search', SearchView.as_view(), name='search-view'),
     path('families', FamilyListView.as_view(), name='family-list'),
     path('genera', GenusListView.as_view(), name='genus-list'),
