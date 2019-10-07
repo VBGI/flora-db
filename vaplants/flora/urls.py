bd87e0059b99a8a98d75cef0a51c27eb6a00894a
Kislov Dmitry <kislov@easydan.com>
2019-10-07 14:04:41 +1000

    Family and genus are accessed via slug instead of pk

--- vaplants/flora/urls.py
+++ vaplants/flora/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from flora.views import (FamilyDetailView, GenusDetailView, SpeciesDetailView,
                         FamilyListView, GenusListView, SpeciesListView, 
-                        SearchView, PageView, OccurrenceDetailView)
+                        PageView, OccurrenceDetailView, SearchView)
 
 urlpatterns = [
-    path('family/<int:pk>', FamilyDetailView.as_view(), name='family-detail'),
-    path('genus/<int:pk>', GenusDetailView.as_view(), name='genus-detail'),
+    path('family/<slug:name>', FamilyDetailView.as_view(), name='family-detail'),
+    path('genus/<slug:name>', GenusDetailView.as_view(), name='genus-detail'),
     path('species/<int:pk>', SpeciesDetailView.as_view(), name='species-detail'),
     path('occurrence/<int:pk>', OccurrenceDetailView.as_view(), name='occurrence-detail'),
     path('search', SearchView.as_view(), name='search-view'),
